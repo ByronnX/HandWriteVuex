92cf6371470de684ab0423c3719ab79f85b63a59
Byronnn <1031466692@qq.com>
2024-02-12 17:14:17 +0800

    Vuex 的模块收集

--- src/Vuex/store.js
+++ src/Vuex/store.js
@@ -15,7 +15,8 @@ export class Store {
     // 封装写法
     /**
      * 1. 把getters这个对象传进去，并把这个对象的key、value都给拿出来调用别的函数
-     * 2. 设置：把getters的key（也就是方法名）作为computed对象的其中一项，并且值为一个函数，返回并执行一个value（方法体）并且把这个实例的state给传进去
+     * 2. 设置：把getters的key（也就是方法
+     * 名）作为computed对象的其中一项，并且值为一个函数，返回并执行一个value（方法体）并且把这个实例的state给传进去
      * 3. 获取：由于在下面进行了代理，所以获取的时候，在实例身上就可以拿到
      */
     foreach(getters, (key, value) => {
