43d6d1609a5f4d83c0a2ad4c301bb4715a5398de
Byronnn <1031466692@qq.com>
2024-02-17 16:18:03 +0800

    Vuex辅助函数的用法

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,63 @@
 <template>
   <div class="hello">
-    <br> 根模块测试： <br>
-    商品数量: {{this.$store.state.num}} 个<br>
-    商品单价: 10 元<br>
-    订单金额: {{this.$store.getters.getPrice}} 元<br>
-    <button @click="$store.commit('changeNum',5)">同步更新：数量+5</button>
-    <button @click="$store.dispatch('changeNum',-5)">异步更新：数量-5</button>
-
-    <br> 子模块测试： <br>
-    A 模块-商品数量: {{this.$store.state.moduleA.num}} 个<br>
-    B 模块-商品数量: {{this.$store.state.moduleB.num}} 个<br>
-    C 模块-商品数量: {{this.$store.state.moduleA.moduleC.num}} 个<br>
-    <button @click="$store.commit('moduleA/changeNum',5)">A 模块-同步更新：数量+5</button>
-    <button @click="$store.commit('moduleB/changeNum',5)">B 模块-同步更新：数量+5</button>
-    <button @click="$store.commit('moduleA/moduleC/changeNum',5)">C 模块-同步更新：数量+5</button>
-
-
+    商品数量: {{ num }} 个<br>
+      商品单价: 10 元<br>
+      订单金额: {{ getPrice }} 元<br>
+      <button @click="mutationChangeNum(5)">同步更新：数量+5</button>
+      <button @click="actionChangeNum(-5)">异步更新：数量-5</button>
+      <br>
+       A 模块-商品数量: {{ numA }} 个<br>
+      C 模块-商品数量: {{ numC }} 个<br>
+      <button @click="mutationAChangeNum(5)">A 模块-同步更新：数量+5</button>
+      <button @click="mutationCChangeNum(5)">C 模块-同步更新：数量+5</button>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapMutations, mapActions, mapState } from 'vuex';
+import { createNamespacedHelpers } from 'vuex';
+const { mapState: mapStateA, mapMutations: mapMutationsA } = createNamespacedHelpers('moduleA')
+const { mapState: mapStateC, mapMutations: mapMutationsC } = createNamespacedHelpers('moduleA/moduleC')
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+
+    }
+  },
   props: {
     msg: String
   },
   mounted() {
     console.log('home mounted:如果有值就代表传递下去了', this.$store);
-
+  },
+  computed: {
+    ...mapState({
+      num: state => state.num,
+    }),
+    ...mapGetters({
+      getPrice: 'getPrice'
+    }),
+    ...mapStateA({
+      numA: state => state.num
+    }),
+    ...mapStateC({
+      numC: state => state.num
+    }),
+  },
+  methods: {
+    ...mapMutations({
+      mutationChangeNum: 'changeNum'
+    }),
+    ...mapActions({
+      actionChangeNum : 'changeNum'
+    }),
+    ...mapMutationsA({
+      mutationAChangeNum: 'changeNum'
+    }),
+    ...mapMutationsC({
+      mutationCChangeNum: 'changeNum'
+    })
   }
 }
 </script>
